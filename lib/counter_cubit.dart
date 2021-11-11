786453b8af595d29268b1d46acdd3aee485d7b18
Mugacha Gichamba <mugacha@Mugachas-MacBook-Pro.local>
2021-11-11 05:23:02 +0300

    updated UI

--- lib/counter_cubit.dart
+++ lib/counter_cubit.dart
@@ -4,11 +4,15 @@ import 'package:meta/meta.dart';
 part 'counter_state.dart';
 
 //counter cubit to emit different states
+
 class CounterCubit extends Cubit<CounterState> {
-  // set initial state of counter cubit to zero
+  // set initial/ default state of counter cubit to zero in
+  // CounterCubit constructor
   CounterCubit() : super(CounterState(counterValue: 0));
 
   // functions to emit new counter states with new counter values passed to their constructors
+  // functions will be called from the UI
+  // state keyword access current state of cubit
   void increment() => emit(CounterState(counterValue: state.counterValue + 1));
   void decrement() => emit(CounterState(counterValue: state.counterValue - 1));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -13,8 +13,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    // creates and provides a single and unique instance
+    // of the counter cubit and make it available below
+    // material app subtree widget
+    // dependency injection widget
+    // use for both bloc and cubit
+    //since cubit is a small part of a bloc
     return BlocProvider<CounterCubit>(
       // take current context and return a counter cubit instance
+      // context - context in which a specific widget is built
       create: (context) => CounterCubit(),
       // instance of counter cubit available below material app widget
       child: MaterialApp(
@@ -63,9 +70,46 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               'You have pushed the button this many times:',
             ),
+            // widget to rebuild ui based on some bloc state changes
+            // wrap exact part of the ui you want to rebuild
             BlocBuilder<CounterCubit, CounterState>(
+              //  builder function must be a pure function
+              // return a widget which only depends on centext and state and
+              // nothing more
               builder: (context, state) {
+                //show new counterValue depending on different emmitted counter states
+                // states emerging from button taps in the UI
+                if(state.counterValue > 0){
+                  return Text(
+                    //toString() since value is an integer
+                    "Positive Baridii!! ${state.counterValue.toString()}",
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .headline4,
+                  );
+                } else if (state.counterValue < 0){
+                  return Text(
+                    //toString() since value is an integer
+                    "Negative Baridii!! ${state.counterValue.toString()}",
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .headline4,
+                  );
+                } else {
+                  return Text(
+                    //toString() since value is an integer
+                    "Sufuri Baridii!! ${state.counterValue.toString()}",
+                    style: Theme
+                        .of(context)
+                        .textTheme
+                        .headline4,
+                  );
+                }
+
                 return Text(
+                  //toString() since value is an integer
                   state.counterValue.toString(),
                   style: Theme
                       .of(context)
@@ -79,6 +123,7 @@ class _MyHomePageState extends State<MyHomePage> {
               children: [
                 FloatingActionButton(
                   onPressed: () {
+                    // this is how you access the cubit instance
                     // increment one to current state and emmit new value
                     BlocProvider.of<CounterCubit>(context).decrement();
                     //     context.bloc<CounterCubit>().decrement();
