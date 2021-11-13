233b7274732173ceb47333cc6173091948aac0bd
Mugacha Gichamba <mugacha@Mugachas-MacBook-Pro.local>
2021-11-13 15:12:12 +0300

    bloc to bloc communication through BlocListener

--- lib/logic/cubit/counter_cubit.dart
+++ lib/logic/cubit/counter_cubit.dart
@@ -10,34 +10,10 @@ part 'counter_state.dart';
 //counter cubit to emit different states
 
 class CounterCubit extends Cubit<CounterState> {
-  final InternetCubit internetCubit;
-  StreamSubscription internetStreamSubscription;
-
-  CounterCubit({@required this.internetCubit}) : super(CounterState(counterValue: 0)) {
-    monitorInternetCubit();
-  }
-
-  void monitorInternetCubit() {
-    internetStreamSubscription = internetCubit.listen((internetState) {
-      if (internetState is InternetConnected &&
-          internetState.connectionType == ConnectionType.Wifi) {
-        increment();
-      } else if (internetState is InternetConnected &&
-          internetState.connectionType == ConnectionType.Mobile) {
-        decrement();
-      }
-    });
-  }
+  CounterCubit() : super(CounterState(counterValue: 0)) {}
 
   void increment() => emit(
       CounterState(counterValue: state.counterValue + 1, wasIncremented: true));
   void decrement() => emit(CounterState(
       counterValue: state.counterValue - 1, wasIncremented: false));
-
-  @override
-  Future<void> close() {
-    // TODO: implement close
-    internetStreamSubscription.cancel();
-    return super.close();
-  }
 }
